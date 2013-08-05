ca5888b84508256fe759137994face0d002a3700
michalxo <michalxo@gmail.com>
2013-08-05 13:30:43 +0200

    Modified basic script. Working Python <-> Java project. (JavaParser) using py4j.

--- Log-change/aux-functions.py
+++ Log-change/aux-functions.py
@@ -1,9 +1,11 @@
 # Auxiliary and test functions 
 import re
 import os
+import ast
 
 BASE_PATH="/home/mtoth/skola/dp/hadoop-common"
 LOG_FILE="/home/mtoth/skola/dp/LogFilterBase/Log-change/hadoop-rewritten-logs-short"
+NEW_LOG_FILE="/home/mtoth/Desktop/hadoop-fresh-logs.txt"
 
 # Merge OLD and NEW "output-log-file" from idea 11 to idea 12 log search export.
 # Also insert new logs to appropriate position. (DOES NOT DO UPDATE!!)
@@ -114,9 +116,15 @@ def remove_test_classes(filepath):
                 space_counter = space_counter + 1
             else:
                 found_first_letter = True;
-                parsed_line += letter #sys.stdout.write(letter)
+                parsed_line += letter
 
         parsed_line = parsed_line[:-1]  # remove last \n
+
+        if space_counter == 4:
+            if line.strip().startswith("Test"):
+                remove_list.append((l_number, -1))
+                print 
+            print "TEST begins", line
 
         if space_counter == 16:
             if remove_mode:
@@ -130,18 +138,20 @@ def remove_test_classes(filepath):
         l_number = l_number + 1
         lines.append((l_number, line))
     
-    print remove_list
     fr.close()    
 
+    print remove_list
     print "number of lines=", len(lines)
     
     fr = open(filepath + "-no-tests", 'w')
 
     to_remove = []
     for removal in remove_list:
+        if removal[1] == -1:
+            removal = (removal[0], l_number)
         for i in range(removal[0], removal[1]):
             print i
-            to_remove.append(i)
+            to_remove.append(i+1)
 
     for i, line in lines:
         if i not in to_remove:
@@ -210,39 +220,143 @@ def search_my_logs():
         if match:
             print match.group()
 
+def findnth(haystack, nth, splitter):
+    parts = haystack.split(splitter, nth+1)
+    return len(haystack)-len(parts[-1])-1
+
+
 def regexp_search():
     l = [
      '"dsadasd" + test, sad + "dsad"',
-     '"Failed to load state.", e, asd, " blabolasd."',
-     '"Unknown child node with name: " + childNodeName',  # !!!!!!  string + string
+     '"Failed to load a state.", e, asd, " blabolasd."',
+     '"Unknown child node with the name: " + childNodeName',  # !!!!!!  string + string
      '"User " + user + " removed from activeUsers, currently: " + var2',
      '"update:" + " application=" + applicationId + " request=" + request',
      '"Test" + test.id() + " test 2=" + max.id().getStuff() +',
      'max.getId(), error',
      'blabol, bla2',
      '"ffd"',
-     '"comment 1 +"'
-    ]
+     '"comment 1 +"',
+     '"  classpath = " + System.getProperty("java.class.path")',
+     '"  args = " + Arrays.asList(args),"  version = " + VersionInfo.getVersion(),\
+            "  classpath = " + System.getProperty("java.class.path"),\
+            "  build = " + VersionInfo.getUrl("asd") + " -r "',
+]
+    # MEGALOG
+    ''' 
+    "Initializing " + queueName + "\n" +
+        "capacity = " + capacity +
+        " [= (float) configuredCapacity / 100 ]" + "\n" + 
+        "asboluteCapacity = " + absoluteCapacity +
+        " [= parentAbsoluteCapacity * capacity ]" + "\n" +
+        "maxCapacity = " + maximumCapacity +
+        " [= configuredMaxCapacity ]" + "\n" +
+        "absoluteMaxCapacity = " + absoluteMaxCapacity +
+        " [= 1.0 maximumCapacity undefined, " +
+        "(parentAbsoluteMaxCapacity * maximumCapacity) / 100 otherwise ]" + 
+        "\n" +
+        "userLimit = " + userLimit +
+        " [= configuredUserLimit ]" + "\n" +
+        "userLimitFactor = " + userLimitFactor +
+        " [= configuredUserLimitFactor ]" + "\n" +
+        "maxApplications = " + maxApplications +
+        " [= configuredMaximumSystemApplicationsPerQueue or" + 
+        " (int)(configuredMaximumSystemApplications * absoluteCapacity)]" + 
+        "\n" +
+        "maxApplicationsPerUser = " + maxApplicationsPerUser +
+        " [= (int)(maxApplications * (userLimit / 100.0f) * " +
+        "userLimitFactor) ]" + "\n" +
+        "maxActiveApplications = " + maxActiveApplications +
+        " [= max(" + 
+        "(int)ceil((clusterResourceMemory / minimumAllocation) * " + 
+        "maxAMResourcePerQueuePercent * absoluteMaxCapacity)," + 
+        "1) ]" + "\n" +
+        "maxActiveAppsUsingAbsCap = " + maxActiveAppsUsingAbsCap +
+        " [= max(" + 
+        "(int)ceil((clusterResourceMemory / minimumAllocation) *" + 
+        "maxAMResourcePercent * absoluteCapacity)," + 
+        "1) ]" + "\n" +
+        "maxActiveApplicationsPerUser = " + maxActiveApplicationsPerUser +
+        " [= max(" +
+        "(int)(maxActiveApplications * (userLimit / 100.0f) * " +
+        "userLimitFactor)," +
+        "1) ]" + "\n" +
+        "usedCapacity = " + usedCapacity +
+        " [= usedResourcesMemory / " +
+        "(clusterResourceMemory * absoluteCapacity)]" + "\n" +
+        "absoluteUsedCapacity = " + absoluteUsedCapacity +
+        " [= usedResourcesMemory / clusterResourceMemory]" + "\n" +
+        "maxAMResourcePerQueuePercent = " + maxAMResourcePerQueuePercent +
+        " [= configuredMaximumAMResourcePercent ]" + "\n" +
+        "minimumAllocationFactor = " + minimumAllocationFactor +
+        " [= (float)(maximumAllocationMemory - minimumAllocationMemory) / " +
+        "maximumAllocationMemory ]" + "\n" +
+        "numContainers = " + numContainers +
+        " [= currentNumContainers ]" + "\n" +
+        "state = " + state +
+        " [= configuredState ]" + "\n" +
+        "acls = " + aclsString +
+        " [= configuredAcls ]" + "\n" + 
+        "nodeLocalityDelay = " +  nodeLocalityDelay + "\n"'''
+    
+    banned_words = ["THE", "A", "AN"]
+    METHOD_NAME_SEPARATORS = 4
 
     for s in l:
-        #           string         variable(s)     string
-        pattern = r'(?:\"[^"]+\")*(?P<var>[^"]+)(?:\"[^"]+\")*|(?:\"[^"]+\")'
-        found =  re.findall(pattern, s, re.IGNORECASE)
+        #           match.any("string.property")                     string(don't want)         variable(s)     string(dont want)
+        pattern = r'(?P<var2>[\w]+[\.[\w]+]?\(\"[\w]+(?:\.[\w]+)*\"\))|'
+        pattern = r'(?:\"[^"]+\")*(?P<var>[^"]+)(?:\"[^"]+\")*|(?:\"[^"]+\")'            
+        #found =  re.findall(pattern, s, re.IGNORECASE)
+
+
+
+        comment_pattern = r'\"([^"]+)\"'#(?:[^"]+)(\"[^"]+\")+'
+        found =  re.findall(comment_pattern, s, re.IGNORECASE)
+        messages = []        
+
+        for f in found:          
+            f = re.sub(r'[\W]+(?!" ")', " ", f.strip().upper())      
+            f = re.sub(" ", "_", f.strip())
+            messages.append(f)
         
-        variables = []
-        if found != ['']:        
-            for found_var in found: 
-                if found_var.strip() != '':
-                    if "+" in found_var:
-                        found_var = found_var.replace("+", "").strip()
+        # print found, "--->", messages  
 
-                    if "," in found_var:
-                        variables = [v.strip() for v in found_var.split(",") if v.strip() != '']
 
-                    elif found_var != '':                    
-                        variables.append(found_var.strip())
+        if messages != []:
+            out = ""
+            for m in messages:                
+                message_list = m.split("_")
+                # remove short or banned words from method names
+                for x in message_list:                    
+                    if len(x) <= 1:
+                        continue
+                    if x in banned_words:
+                        continue
+                    out = out + x + "_"                
+
+                                        
+            if out.endswith("_"):
+                out = out[:-1]
+                if out.count("_")-1 >= METHOD_NAME_SEPARATORS:                    
+                    out = out[:findnth(out, METHOD_NAME_SEPARATORS, "_")]
+            print "out=", out
+
+
+        #variables = []
+        #if found != ['']:   
+        #    print found     
+        #     for found_var in found: 
+        #         if str(found_var).strip() != '':
+        #             if "+" in found_var:
+        #                 found_var = found_var.replace("+", "").strip()
+
+        #             if "," in found_var:
+        #                 variables = [v.strip() for v in found_var.split(",") if v.strip() != '']
+
+        #             elif found_var != '':                    
+        #                 variables.append(found_var.strip())
                    
-        print found, "---->", variables
+        # print found, "---->", variables
 
 def is_my_log(line):
     # "LOG.SOMETHING_INTERESTING("
@@ -308,6 +422,27 @@ def test():
 
     print slovnik.items()
 
-test()
 
+
+    for x in slovnik.keys():
+        for val in slovnik[x]:
+            print "va=", val
+
+
+def ast_tree(filepath):
+    f = open(filepath, "r")
+    output =  '''
+LOG.info("Giving handle (fileId:" + handle.getFileId()
+    + ") to client for export " + path);        
+    '''
+    
+    print output
+    print ast.dump(ast.parse(output))
+
+regexp_search()
+
+
+JAVA_FILE = "/home/mtoth/skola/dp/hadoop-common/hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/util/VersionInfo.java"
+JAVA_FILE = "/home/mtoth/Desktop/fds"
+#ast_tree(JAVA_FILE)
 
